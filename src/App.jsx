87624a52e4df5577a150158a7c46968262f50bcb
Ivanaricci <ivana.ricci26@gmail.com>
2025-05-12 18:06:36 +0200

    [UPD] Done filtered

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ const arrayFilms = [
 
 function App() {
 
+  const [selectedGenre, setSelectedGenre] = useState("");
+  const [filmFiltered, setFilmFiltered] = useState(arrayFilms);
+
   const generiUnici = [];
 
   for (let i = 0; i < arrayFilms.length; i++) {
@@ -22,6 +25,13 @@ function App() {
     }
   }
 
+  useEffect(() => {
+    const filteredArray = arrayFilms.filter((film) => {
+      return film.genre.includes(selectedGenre)
+    });
+    setFilmFiltered(filteredArray);
+  }, [selectedGenre])
+
   return (
     <>
       <div className="container">
@@ -29,7 +39,9 @@ function App() {
           <div className="col12">
             <h1>Lista Film</h1>
           </div>
-          <select class="form-select" aria-label="Default select example">
+          <select class="form-select mb-4" aria-label="Default select example"
+            value={selectedGenre}
+            onChange={e => setSelectedGenre(e.target.value)}>
             <option selected>Cerca per genere</option>
             {generiUnici.map((genere) => {
               return (
@@ -43,7 +55,7 @@ function App() {
           </select>
           <div className="col12">
             <ul className="list-group">
-              {arrayFilms.map((film) => {
+              {filmFiltered.map((film) => {
                 return (
                   <li className='list-group-item' key={film.id}>
                     <h5>{film.title}</h5>
